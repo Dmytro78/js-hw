4a79887da4ba65beab59948a2754f7f55dfd62db
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-04-12 21:27:44 +0300

    start

---
+++ js/m-01.js
@@ -0,0 +1,58 @@
+/*-------1---------*/
+const productName = 'Droid';
+const pricePerItem = 2000;
+
+/*-------2---------*/
+// Start code
+let productName = 'Droid';
+let pricePerItem = 2000;
+
+// Write your code under this line
+pricePerItem=3500;
+productName='Repair droid'
+
+/*-------3---------*/
+const topSpeed=160;
+const distance=617.54;
+const login='mango935';
+const isOnline=true;
+const isAdmin = false;
+
+/*-------4---------*/
+// Базовый код
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Пиши код ниже этой строки
+const totalPrice = (pricePerItem*orderedQuantity);
+
+/*-------5---------*/
+// Базовый код
+const productName = 'Дроид';
+const pricePerItem = 3500;
+
+// Пиши код ниже этой строки
+const message=`Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+
+/*-------6---------*/
+const pricePerDroid=800;
+const orderedQuantity=6;
+const deliveryFee=50;
+const totalPrice=(pricePerDroid*orderedQuantity+deliveryFee);
+const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+//точки должны быть на англ. раскладке
+
+/*-------7---------*/
+// Пиши код ниже этой строки
+function sayHi() {
+  console.log('Привет, это моя первая функция!');}
+sayHi();
+
+/*-------8---------*/
+
+
+/*-------9---------*/
+/*-------10---------*/
+/*-------11---------*/
+/*-------12---------*/
