28b7a9f9a0d066c2a6ed8e081807171bfaecc8f5
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-05-02 22:04:48 +0300

    25-03

--- js/add.js
+++ js/add.js
@@ -1,3 +1,21 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
 
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+  const totalPrice = 0;
+  
+	for (const product of products) {
+        if (productName === products.name) {
+            totalPrice.push(products[price]*products[quantity]);
+        }
+        }
+      
+     return totalPrice;
 
-
+  // Пиши код выше этой строки
+}
